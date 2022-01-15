7a6437c65b619b390d2076d8755a6fcbe8b7af02
zhangsan <zhangsan@qq.com>
2022-01-15 20:45:24 +0800

    second commit

--- Hello/src/main/java/com/demo/Hello.java
+++ Hello/src/main/java/com/demo/Hello.java
@@ -2,5 +2,7 @@ package com.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
