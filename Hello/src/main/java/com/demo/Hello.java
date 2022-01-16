0e6aaea92b422ec1397eee2049d24df04ff8cdc8
yuhaoplus <97778866+yuhaoplus@users.noreply.github.com>
2022-01-16 09:53:21 +0800

    push commit

--- Hello/src/main/java/com/demo/Hello.java
+++ Hello/src/main/java/com/demo/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
         System.out.println("我是hot-fix");
         System.out.println("我是master");
         System.out.println("ssh-push");
+        System.out.println("pull");
     }
 }
